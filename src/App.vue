96258d70fa0d30ffd3f9a31839afb794d48fd57f
mark5041 <mark.beloso5041@gmail.com>
2022-01-13 16:59:04 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Main />
+    <Foot />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Foot from './components/Foot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+    Foot,
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+@import "./assets/scss/style.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="nav">
+      <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+            </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+    data(){
+        return {
+            title: "Lorem Ipsum"
+        }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+    .nav {
+        height: 100px;
+    }
+
+    .logo {
+        flex-grow: 4;
+        img {
+            width: 60px;
+        }
+    }
+
+    ul {
+        list-style: none;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        li {
+            display: flex;
+            align-items: center;
+            text-transform: uppercase;
+            margin: 0 1em 0 1em;
+            a {
+                text-decoration: none;
+                display: inline-block;
+                font-weight: 800;
+                font-size: 1em;
+                height: 100%;
+                line-height: 100px;
+                color: black;
+                border-bottom: 0.4em solid transparent;
+                &.active,
+                &:hover {
+                    color: $bluehover;
+                    border-bottom: 0.4em solid $bluehover;
+                }
+            }
+            
+        }
+    }
+</style>
